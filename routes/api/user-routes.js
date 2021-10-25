7f38d1d8262fa3086acadbc8fbb969a1623cd13f
nguyen-william93 <=>
2021-10-25 14:40:27 -0500

    create and finish login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,7 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
@@ -58,6 +59,28 @@ router.put('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     })
+});
+
+//login route
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData){
+            res.status(400).json({ message: "No user with that email address"});
+            return;
+        }
+        // res.json({ user: dbUserData});
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({ message: "Incorrect"});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: "You are now Log in"});
+    });
 });
 
 // DELETE /api/users/1
