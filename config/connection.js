4b44241ad05c6ec73c60e923420272d964166072
nguyen-william93 <=>
2021-11-01 14:39:21 -0500

    current working version

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-//create connection to your database, pass in your mysql information.
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'vnn195245', {
-    host: "localhost",
-    dialect: "mysql",
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
     port: 3306
-});
+  });
+}
 
 module.exports = sequelize;
