07614966ebb3934f75707ccbe46baa399209f39d
nguyen-william93 <=>
2021-11-03 22:07:56 -0500

    bug on delete

--- models/Vote.js
+++ models/Vote.js
@@ -1,19 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Vote extends Model {}
+class Vote extends Model { }
 
 Vote.init(
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      allowNull: false,
       autoIncrement: true
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'user',
         key: 'id'
@@ -21,7 +19,6 @@ Vote.init(
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'post',
         key: 'id'
